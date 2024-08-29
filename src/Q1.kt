bca955bc858946bbe75369e056f000ae3a8ae951
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-08-29 14:18:26 +0530

    Q1) Factorials

--- src/Q1.kt
+++ src/Q1.kt
@@ -1,9 +1,9 @@
 fun main() {
     print("Enter the number: ")
-    val number= readlnOrNull()?.toInt()
-    var factorial=1
-    // number!! means non-null variable
-    for (i in 1..number!!){
+    val number= readln().toInt()
+    //used LongInt (L) for storing value uptil 20 factorials
+    var factorial=1L
+    for (i in 1..number){
         factorial*=i
     }
     println("Factorial of $number is : $factorial")
