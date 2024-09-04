5443e08dc6c5507472e5d26fed0a948991879cae
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-09-04 19:23:57 +0530

    Q3) Perfect Numbers

--- src/Q3.kt
+++ src/Q3.kt
@@ -1,12 +1,12 @@
 fun main(){
     print("Enter the number: ")
-    var number = readLine()?.toInt()
+    var number = readln().toInt()
     var sum=0
     if(number==1 || number==0){
         println("$number is not a Perfect Number since they have only itself as a factor.")
         return
     }
-    for (i in 1 ..< number!!){
+    for (i in 1 ..< number){
         if(number%i == 0){
             sum+=i
         }
