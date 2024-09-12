715d42c729098f1daaadac6ffa40f40f54214038
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-09-12 16:13:30 +0530

    Classes in Kotlin

--- src/Student.kt
+++ src/Student.kt
@@ -1,26 +1,40 @@
-class Student {
-    private var isPass: Boolean = false
+//example program: Create a student class to display whether a student has passed or failed
+//------------------------------------------------------------------------------------------
+//class Student {
+//    private var isPass: Boolean = false
+//
+//    fun studyTrue(){
+//        isPass=true
+//    }
+//    fun studyFalse(){
+//        isPass=false
+//    }
+//    fun studentResult(s:String){
+//        if (isPass){
+//            println("$s Student has Passed.")
+//        }else{
+//            println("$s Student did not pass.")
+//        }
+//    }
+//}
+//
+//fun main(){
+//    val s1=Student()
+//    val s2=Student()
+//    s1.studyTrue()
+//    s2.studyFalse()
+//    s1.studentResult("s1")
+//    s2.studentResult("s2")
+//}
 
-    fun studyTrue(){
-        isPass=true
-    }
-    fun studyFalse(){
-        isPass=false
-    }
-    fun studentResult(s:String){
-        if (isPass){
-            println("$s Student has Passed.")
-        }else{
-            println("$s Student did not pass.")
-        }
-    }
+//----------------------------------------------------------------------
+//Example for Primary Constructor
+//----------------------------------------------------------------------
+fun main(args: Array<String>){
+    val stud1= Student("David",500)
+    println("First Name: ${stud1.firstName}")
+    println("Marks: ${stud1.marks}")
 }
-
-fun main(){
-    val s1=Student()
-    val s2=Student()
-    s1.studyTrue()
-    s2.studyFalse()
-    s1.studentResult("s1")
-    s2.studentResult("s2")
+// we are using this class to call our (parameterized) "Student" constructor.
+class Student(val firstName:String, val marks:Number){
 }
